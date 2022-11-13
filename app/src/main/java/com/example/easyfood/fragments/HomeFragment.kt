1f6cc50d3a094e0a914189798325cb9c94764ba9
BaileyEggebrecht <baileyteggebrecht@gmail.com>
2022-11-13 13:49:50 -0700

    Click Cardview to access details finsihed

--- app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
@@ -1,15 +1,18 @@
 package com.example.easyfood.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.easyfood.R
+import com.example.easyfood.activities.MealActivity
 import com.example.easyfood.databinding.FragmentHomeBinding
 import com.example.easyfood.pojo.Meal
 import com.example.easyfood.pojo.MealList
@@ -43,9 +46,18 @@ class HomeFragment : Fragment() {
 
         homeMvvm.getRandomMeal()
         observeRandomMeal()
+        onRandomMealClick()
 
 
     }
+
+    private fun onRandomMealClick() {
+
+        binding.randomMealCard.setOnClickListener {
+            val intent = Intent(activity, MealActivity::class.java)
+            startActivity(intent)
+    }
+}
 
     private fun observeRandomMeal() {
         homeMvvm.observeRandomMealLiveData().observe(viewLifecycleOwner, object : Observer<Meal>{
