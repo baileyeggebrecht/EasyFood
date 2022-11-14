5d1b1df9e07b532236dca9f9e15e055f41be4dd7
BaileyEggebrecht <baileyteggebrecht@gmail.com>
2022-11-14 14:07:03 -0700

    Completed Meal View (Youtube functionality)

--- app/src/main/java/com/example/easyfood/retrofit/MealApi.kt
+++ app/src/main/java/com/example/easyfood/retrofit/MealApi.kt
@@ -3,10 +3,14 @@ package com.example.easyfood.retrofit
 import android.telecom.Call
 import com.example.easyfood.pojo.MealList
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MealApi {
 
     @GET("random.php")
     fun getRandomMeal(): retrofit2.Call<MealList>
+
+    @GET("lookup.php?")
+    fun getMealDetails(@Query("i") id:String) : retrofit2.Call<MealList>
 
 }
