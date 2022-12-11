cf72768fab0873b38060e4cda998939af65a4df4
BaileyEggebrecht <baileyteggebrecht@gmail.com>
2022-12-11 14:36:13 -0700

    Messed some stuff up and fixed it

---
+++ app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
@@ -0,0 +1,152 @@
+package com.example.easyfood.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.easyfood.activities.CategoryMealsActivity
+import com.example.easyfood.activities.MealActivity
+import com.example.easyfood.adapters.CategoriesAdapter
+import com.example.easyfood.adapters.MostPopularAdapter
+import com.example.easyfood.databinding.FragmentHomeBinding
+import com.example.easyfood.fragments.HomeFragment.Companion.MEAL_ID
+import com.example.easyfood.fragments.HomeFragment.Companion.MEAL_NAME
+import com.example.easyfood.fragments.HomeFragment.Companion.MEAL_THUMB
+import com.example.easyfood.pojo.MealsByCategory
+import com.example.easyfood.pojo.Meal
+import com.example.easyfood.viewModel.HomeViewModel
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var homeMvvm:HomeViewModel
+    private lateinit var randomMeal:Meal
+    private lateinit var popularItemsAdapter:MostPopularAdapter
+    private lateinit var categoriesAdapter:CategoriesAdapter
+
+    companion object {
+        const val MEAL_ID = "com.example.easyfood.fragment.idMeal"
+        const val MEAL_NAME = "com.example.easyfood.fragments.nameMeal"
+        const val MEAL_THUMB = "com.example.easyfood.fragments.thumbMeal"
+        const val CATEGORY_NAME = "com.example.easyfood.fragments.categoryName"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        homeMvvm = ViewModelProvider( this)[HomeViewModel::class.java]
+        popularItemsAdapter = MostPopularAdapter()
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        preparePopularItemsRecyclerView()
+
+        homeMvvm.getRandomMeal()
+        observeRandomMeal()
+        onRandomMealClick()
+
+        homeMvvm.getPopularItems()
+        observePopularItemsLiveData()
+        onPopularItemClick()
+
+
+        prepareCategoriesRecyclerView()
+        homeMvvm.getCategories()
+        observeCategoriesLiveData()
+        onCategoryClick()
+
+
+
+
+    }
+
+    private fun onCategoryClick() {
+        categoriesAdapter.onItemClick = { category ->
+            val intent = Intent(activity, CategoryMealsActivity::class.java)
+            intent.putExtra(CATEGORY_NAME, category.strCategory)
+            startActivity(intent)
+        }
+    }
+
+    private fun prepareCategoriesRecyclerView() {
+        categoriesAdapter = CategoriesAdapter()
+        binding.recViewCategories.apply {
+            layoutManager = GridLayoutManager(context, 3, GridLayoutManager.VERTICAL, false)
+            adapter = categoriesAdapter
+
+        }
+    }
+
+    private fun observeCategoriesLiveData() {
+        homeMvvm.observeCategoriesLiveData().observe(viewLifecycleOwner, Observer { categories ->
+                categoriesAdapter.setCategoryList(categories)
+
+        })
+    }
+
+    private fun onPopularItemClick() {
+        popularItemsAdapter.onItemClick = { meal->
+            val intent = Intent(activity, MealActivity::class.java)
+            intent.putExtra(MEAL_ID, meal.idMeal)
+            intent.putExtra(MEAL_NAME, meal.strMeal)
+            intent.putExtra(MEAL_THUMB, meal.strMealThumb)
+            startActivity(intent)
+
+        }
+    }
+
+    private fun preparePopularItemsRecyclerView() {
+        binding.recViewMealsPopular.apply {
+            layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+            adapter = popularItemsAdapter
+        }
+    }
+
+    private fun observePopularItemsLiveData() {
+        homeMvvm.observePopularItemsLiveData().observe(viewLifecycleOwner
+        ) { mealList->
+            popularItemsAdapter.setMeals(mealsList = mealList as ArrayList<MealsByCategory> /* = java.util.ArrayList<com.example.easyfood.pojo.CategoryMeals> */)
+        }
+    }
+
+    private fun onRandomMealClick() {
+
+        binding.randomMealCard.setOnClickListener {
+            val intent = Intent(activity, MealActivity::class.java)
+            intent.putExtra(MEAL_ID, randomMeal.idMeal)
+            intent.putExtra(MEAL_NAME, randomMeal.strMeal)
+            intent.putExtra(MEAL_THUMB, randomMeal.strMealThumb)
+            startActivity(intent)
+    }
+}
+
+    private fun observeRandomMeal() {
+        homeMvvm.observeRandomMealLiveData().observe(viewLifecycleOwner,
+            { meal ->
+                Glide.with(this@HomeFragment)
+                    .load(meal!!.strMealThumb)
+                    .into(binding.imgRandomMeal)
+
+                this.randomMeal = meal
+        })
+    }
+
+}
---
+++ app/src/main/java/com/example/easyfood/retrofit/MealApi.kt
@@ -0,0 +1,25 @@
+package com.example.easyfood.retrofit
+
+import com.example.easyfood.pojo.*
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MealApi {
+
+    @GET("random.php")
+    fun getRandomMeal(): retrofit2.Call<MealList>
+
+    @GET("lookup.php?")
+    fun getMealDetails(@Query("i") id:String) : retrofit2.Call<MealList>
+
+    @GET("filter.php?")
+    fun getPopularItems(@Query("c") categoryName: String) : retrofit2.Call<MealsByCategoryList>
+
+    @GET("categories.php")
+    fun getCategories() : Call<CategoryList>
+
+    @GET("filter.php")
+    fun getMealsByCategory(@Query("c") categoryName: String) : Call<MealsByCategoryList>
+
+}
